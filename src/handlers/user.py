e0b52882110e5db0c61dcf8c3bbedcc1e64c773f
Islam <islam_tramov@mail.ru>
2024-11-30 13:21:05 +0300

    Fix face swap

--- src/handlers/user.py
+++ src/handlers/user.py
@@ -11,12 +11,18 @@ user_router = Router()
 @user_router.message(F.photo)
 async def get_photo_id(message: Message):
     user_id = message.from_user.id
+
+    source_filepath = f"./images/{user_id}.jpg"
+    target_filepath = "./images/jason.jpg"
+    output_filepath = f"./images/{user_id}_result.jpg"
+
     file = await message.bot.get_file(message.photo[-1].file_id)
 
-    await message.bot.download_file(file.file_path, f"{user_id}.jpg")
+    await message.bot.download_file(file.file_path, source_filepath)
+
 
     start_time = time.time()
-    swap_faces(f"{user_id}.jpg", "jason.jpg", f"{user_id}_result.jpg")
+    swap_faces(source_filepath, target_filepath, output_filepath)
     logging.info("--- %s seconds ---" % (time.time() - start_time))
 
-    await message.answer_photo(FSInputFile(f"{user_id}_result.jpg"))
+    await message.answer_photo(FSInputFile(output_filepath))
--- src/utils/face_swap.py
+++ src/utils/face_swap.py
@@ -7,20 +7,34 @@ from insightface.app import FaceAnalysis
 
 
 def download_model():
-    url = 'https://huggingface.co/CountFloyd/deepfake/resolve/main/inswapper_128.onnx'
-    download_file_path = 'inswapper_128.onnx'
-    
+    url = "https://huggingface.co/CountFloyd/deepfake/resolve/main/inswapper_128.onnx"
+    download_file_path = "./models/inswapper_128.onnx"
+
     if not Path(download_file_path).is_file():
         request = urllib.request.urlopen(url)
-        total = int(request.headers.get('Content-Length', 0))
-        with tqdm(total=total, desc='Downloading', unit='B', unit_scale=True, unit_divisor=1024) as progress:
-            urllib.request.urlretrieve(url, download_file_path, reporthook = lambda count, block_size, total_size: progress.update(block_size))
+        total = int(request.headers.get("Content-Length", 0))
+        with tqdm(
+            total=total,
+            desc="Downloading",
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1024,
+        ) as progress:
+            urllib.request.urlretrieve(
+                url,
+                download_file_path,
+                reporthook=lambda count, block_size, total_size: progress.update(
+                    block_size
+                ),
+            )
+
 
 def swap_faces(source_path: str, target_path: str, output_path: str):
 
     download_model()
-    
-    model_filepath = "./inswapper_128.onnx"
+
+    model_filepath = "./models/inswapper_128.onnx"
+
     app = FaceAnalysis(name="buffalo_l")
     app.prepare(ctx_id=0, det_size=(640, 640))
     swapper = insightface.model_zoo.get_model(model_filepath)
@@ -35,7 +49,4 @@ def swap_faces(source_path: str, target_path: str, output_path: str):
     for face in faces:
         swapped_res = swapper.get(swapped_res, face, source_face, paste_back=True)
 
-    image = cv2.cvtColor(swapped_res, cv2.COLOR_BGR2RGB)
-    image = image.astype("float32")
-    # image /= 255.0
-    cv2.imwrite(output_path, image)
+    cv2.imwrite(output_path, swapped_res)
