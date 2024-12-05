89466a83a6f790480c54aa4672b14c35e81a41df
Islam <islam_tramov@mail.ru>
2024-12-05 17:09:51 +0300

    Preventing local save of input image

--- src/handlers/user.py
+++ src/handlers/user.py
@@ -1,9 +1,9 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, URLInputFile, BufferedInputFile
 from ..utils.face_swap import swap_faces
-from aiogram.types import FSInputFile
-from aiogram.types.input_sticker import InputSticker
-from aiogram.enums import StickerFormat
+import io
+
+from ..utils.create_sticker_pack import create_sticker_pack
 
 import logging
 
@@ -12,53 +12,28 @@ user_router = Router()
 
 @user_router.message(F.photo)
 async def get_photo_id(message: Message):
+
     user_id = message.from_user.id
 
-    source_filepath = f"./images/{user_id}.jpg"
+    file_in_io = io.BytesIO()
+    await message.bot.download(file=message.photo[-1].file_id, destination=file_in_io)
 
-    file = await message.bot.get_file(message.photo[-1].file_id)
+    print(file_in_io)
+    await message.answer("Начинаем обработку Вашего изображения")
 
-    await message.bot.download_file(file.file_path, source_filepath)
+    sticker_pack_name = f"sticker_pack_{user_id}_by_dev_814244fb_bot"
 
-    sticker_set_name = f"sticker_pack_{user_id}_by_dev_814244fb_bot"
+    swap_faces(file_in_io, "./images/templates/", user_id)
 
-    swap_faces(source_filepath, "./images/templates/", user_id)
+    await message.answer("Начинаем создавать стикеры")
 
-    stickers = []
-    for i in range(10):
-        output_filepath = f"./images/{user_id}_{i}.jpg"
-        stickers.append(
-            InputSticker(
-                sticker=FSInputFile(output_filepath),
-                format=StickerFormat.STATIC,
-                emoji_list=["☺️"],
-            )
-        )
+    await create_sticker_pack(message, user_id, sticker_pack_name)
 
     try:
-        await message.bot.delete_sticker_set(name=sticker_set_name)
-        logging.info(
-            f"deleted sticker pack with for user: {user_id}, name: {sticker_set_name}"
-        )
-
-    except:
-        logging.info("sticker pack already deleted")
-
-    try:
-        await message.bot.create_new_sticker_set(
-            user_id=user_id,
-            name=sticker_set_name,
-            title="title",
-            stickers=stickers,
-        )
-    except:
-        logging.info("cannot create sticker pack")
-
-    try:
-        sticker_set = await message.bot.get_sticker_set(name=sticker_set_name)
+        sticker_set = await message.bot.get_sticker_set(name=sticker_pack_name)
         await message.answer_sticker(sticker=sticker_set.stickers[0].file_id)
         logging.info("sent sticker pack")
     except:
         logging.info(
-            f"cannot find sticker pack for user: {user_id}, name: {sticker_set_name}"
+            f"cannot find sticker pack for user: {user_id}, name: {sticker_pack_name}"
         )
--- src/utils/face_swap.py
+++ src/utils/face_swap.py
@@ -1,9 +1,17 @@
 import cv2
 import urllib
 import insightface
+import numpy as np
 from tqdm import tqdm
 from pathlib import Path
 from insightface.app import FaceAnalysis
+from insightface.app.common import Face
+from os import listdir
+from os.path import isfile, join
+import json
+import sys
+import os
+import io
 
 
 def download_model():
@@ -29,24 +37,54 @@ def download_model():
             )
 
 
-def swap_faces(source_path: str, target_path: str, output_path: str):
-
+def swap_faces(source: io.BytesIO, template_dir: str, user_id: int):
     download_model()
+
+    faces_file_path = "./configs/faces_dict.json"
+    with open(faces_file_path) as json_file:
+        loaded_r = json.load(json_file)
+
+    template_files = [
+        file for file in listdir(template_dir) if isfile(join(template_dir, file))
+    ]
+
+    for file in template_files:
+        for face in loaded_r[file]:
+            for key in face.keys():
+                if key in [
+                    "bbox",
+                    "kps",
+                    "landmark_3d_68",
+                    "pose",
+                    "landmark_2d_106",
+                    "embedding",
+                ]:
+                    face[key] = np.array(face[key])
+                elif key in ["det_score"]:
+                    face[key] = float(face[key])
+                elif key in ["gender"]:
+                    face[key] = int(face[key])
+        loaded_r[file][0] = Face(loaded_r[file][0])
 
     model_filepath = "./models/inswapper_128.onnx"
 
+    sys.stdout = open(os.devnull, "w")
     app = FaceAnalysis(name="buffalo_l")
     app.prepare(ctx_id=0, det_size=(640, 640))
     swapper = insightface.model_zoo.get_model(model_filepath)
+    sys.stdout = sys.__stdout__
 
-    source_img = cv2.imread(source_path)
+    nparr = np.frombuffer(source.getvalue(), np.uint8)
+    source_img = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+
     source_face = app.get(source_img)[0]
 
-    res = cv2.imread(target_path)
-    faces = app.get(res)
+    for sticker_i, file in enumerate(template_files):
+        output_filepath = f"./images/{user_id}_{sticker_i}.jpg"
+        res = cv2.imread(template_dir + file)
+        faces = loaded_r[file]
 
-    swapped_res = res.copy()
-    for face in faces:
-        swapped_res = swapper.get(swapped_res, face, source_face, paste_back=True)
+        for face in faces:
+            res = swapper.get(res, face, source_face, paste_back=True)
 
-    cv2.imwrite(output_path, swapped_res)
+        cv2.imwrite(output_filepath, res)
